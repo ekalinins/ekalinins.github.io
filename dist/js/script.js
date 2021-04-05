1879ebf1f77766e6ba6c903ea664f9e65cf4a12f
Egor <pein2112@mail.ru>
2021-04-05 13:13:51 +0300

    adding media

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,9 +21,9 @@ $(document).ready(function(){
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
+            });
         });
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -71,7 +71,7 @@ $(document).ready(function(){
         
             }
         });
-    };
+    }
 
     validateForms('#consultation-form');
     validateForms('#order form');
